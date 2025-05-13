65cbc1c791bc661d65cd9905a295f4d811b0b117
Thiago Renan <thiagorenan1108@gmail.com>
2025-05-13 13:28:01 -0300

    Collections

---
+++ DesafioControleDeFluxo/src/ExcecaoCriada/ParametroIlegalException.java
@@ -0,0 +1,11 @@
+package ExcecaoCriada;
+
+public class ParametroIlegalException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+		public ParametroIlegalException(String message) {
+			super(message);
+		}
+
+}
